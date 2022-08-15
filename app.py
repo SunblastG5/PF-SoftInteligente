80e02f7fefb85149f40e0fe73729600180150c1a
Alexis Arias <alexis.arias555@gmail.com>
2022-08-15 02:22:25 -0500

    Added images

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 from Fuzzy import ControlBrake
+from PIL import Image
+from streamlit.components.v1 import html
+
+image = Image.open('images/image.png')
 st.sidebar.title("Proyecto Final - Software Inteligente")
 with st.sidebar.expander("Información", True):
     st.markdown("""
@@ -18,17 +22,25 @@ with st.sidebar.expander("Información", True):
         * **Ramos Paredes, Roger Anthony**
     """)
 
+st.title("Sistema de control de frenos usando Logica difusa")
 controlBrake = ControlBrake()
+controlBrake.setRules()
+
+body="""<style>
+        div.css-1v0mbdj.etr89bj1 {
+            margin-left: 10%;
+        }
+        </style>
+    """
+st.markdown(body, unsafe_allow_html=True)
+
+st.image(image)
 
 speed = st.slider(label="Seleccione la velocidad", min_value=0.0, max_value=100.0)
 distance = st.slider(label="Seleccione la distancia", min_value=0.0, max_value=60.0)
 
 if st.button("Aceptar"):
-    controlBrake.setRules()
     st.write("Fuerza de freno")
-    st.write(controlBrake.determine(speed,distance))
-
-
-
+    st.write(f'{controlBrake.determine(speed,distance)} N')
 
 
